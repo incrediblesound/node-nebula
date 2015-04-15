d45b5c9d8e00f77392bee7fe5dabc596172d9f56
James Edwards <jamese@syapse.com>
2015-04-14 20:50:15 -0700

    initial commit

---
+++ nebula.js
@@ -0,0 +1,62 @@
+var http = require('http');
+var request_options = {
+    host: '127.0.0.1',
+    port: 1984,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+};
+
+var nebula = {};
+
+nebula.open = function(options){
+	var data = JSON.stringify(options);
+	request_options.path = '/init';
+	request_options.headers['Content-Length'] = Buffer.byteLength(data);
+	var request = http.request(request_options)
+	request.write(data);
+	request.end();
+};
+
+nebula.save = function(query){
+	var data = JSON.stringify(query);
+	request_options.path = '/save';
+	request_options.headers['Content-Length'] = Buffer.byteLength(data);
+	var request = http.request(request_options)
+	request.write(data);
+	request.end();
+}
+
+nebula.saveAll = function(query){
+	var data = JSON.stringify(query);
+	request_options.path = '/saveall';
+	request_options.headers['Content-Length'] = Buffer.byteLength(data);
+	var request = http.request(request_options)
+	request.write(data);
+	request.end();
+}
+
+nebula.query = function(query, cb){
+	var data = JSON.stringify(query);
+	request_options.path = '/save';
+	request_options.headers['Content-Length'] = Buffer.byteLength(data);
+	var request = http.request(request_options, function(res){
+		retrieveData(res, function(data){
+			cb(data);
+		})
+	})
+	request.write(data);
+	request.end();
+}
+
+module.exports = nebula;
+
+function retrieveData(res, cb){
+	res.on('data', function(chunk) {
+        chunk = chunk.toString();
+        chunk = JSON.parse(chunk);
+        cb(chunk);
+    });
+}
+
